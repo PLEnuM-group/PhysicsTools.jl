5e7b286804f0abcf52305fade933becf54ef3d63
Documenter.jl <documenter@juliadocs.github.io>
2023-01-23 13:16:44 +0000

    build based on a6d2946

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PhysicsTools","category":"page"},{"location":"#PhysicsTools","page":"Home","title":"PhysicsTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PhysicsTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PhysicsTools]","category":"page"}]
+}
