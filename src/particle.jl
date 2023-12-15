81549388ef455b95aa2cd278547dd2ede0f00a2a
Arsenije Arsenic <152603285+ArsenijeA@users.noreply.github.com>
2023-12-15 09:03:59 +0100

    Improve documentation for particle.jl (#14)
    * add docstring for repeat_for
    
    * Updated particle.jl documentation
    
    * Updated particle.jl documentation and added is_neutrino(particle) function

--- src/particle.jl
+++ src/particle.jl
@@ -12,27 +12,39 @@ export is_neutrino
 
 abstract type ParticleType end
 
-struct PEPlus <: ParticleType end
-struct PEMinus <: ParticleType end
-struct PGamma <: ParticleType end
-struct PMuPlus <: ParticleType end
-struct PMuMinus <: ParticleType end
-struct PNuE <: ParticleType end
-struct PNuMu <: ParticleType end
-struct PNuTau <: ParticleType end
-struct PNuEBar <: ParticleType end
-struct PNuMuBar <: ParticleType end
-struct PNuTauBar <: ParticleType end
-struct PHadronShower <: ParticleType end
-struct PLightSabre <: ParticleType end
+"Subtype of ParticleType, used to classify particles. pdg_code() can be used to find the corresponding particle PDF code."
+struct PEPlus <: ParticleType end,
+struct PEMinus <: ParticleType end,
+struct PGamma <: ParticleType end,
+struct PMuPlus <: ParticleType end,
+struct PMuMinus <: ParticleType end,
+struct PNuE <: ParticleType end,
+struct PNuMu <: ParticleType end,
+struct PNuTau <: ParticleType end,
+struct PNuEBar <: ParticleType end,
+struct PNuMuBar <: ParticleType end,
+struct PNuTauBar <: ParticleType end,
+struct PHadronShower <: ParticleType end,
+struct PLightSabre <: ParticleType end,
 struct PUnknown <: ParticleType end
 
 const ALL_PARTICLES = [PEPlus, PEMinus, PGamma, PMuPlus, PMuMinus, PNuE, PNuMu, PNuTau, PNuEBar, PNuMuBar, PNuTauBar, PHadronShower, PLightSabre]
 
 abstract type ParticleShape end
-struct Track <: ParticleShape end
+
+"Subtype of ParticleShape, used to classify signatures in detector. PEPlus, PEMinus, PGamma and PHadronShower have the shape Cascade; while PMuMinus, PMuPlus and PLightSabre have the shape Track. particle_shape() can be used to return shape of particle signature."
+struct Track <: ParticleShape end,
 struct Cascade <: ParticleShape end
 
+
+"""
+    pdg_code(particle)
+
+Return integer corresponding to the PDG code of a givern particle. ptype_for_code() can be used to find the struct of the corresponding particle.
+    
+# Arguments
+- `particle::Type`: Particle to return PDG code of.
+"""
 pdg_code(::Type{PEPlus}) = -11
 pdg_code(::Type{PEMinus}) = 11
 pdg_code(::Type{PGamma}) = 22
@@ -47,6 +59,15 @@ pdg_code(::Type{PNuTauBar}) = -16
 pdg_code(::Type{PUnknown}) = 0
 pdg_code(::Type) = 0
 
+
+"""
+    ptype_for_code(code::Integer)
+
+Return particle struct corresponding to a given PDG code. pdg_code() can be used to find the PDG code of a corresponding particle.
+
+# Arguments
+- `code::Integer`: PDG code to get particle struct of. 
+"""
 function ptype_for_code(code::Integer)
     for p in ALL_PARTICLES
         if pdg_code(p) == code
@@ -73,7 +94,19 @@ is_neutrino(::Type{PNuTau}) = true
 is_neutrino(::Type{PNuTauBar}) = true
 
 
+"""
+    Particle{T,PType<:ParticleType}
 
+Struct containing information of a single particle
+
+# Fields
+- `position::SVector{3,T}`: Particle position given as a three dimensional static vector in detector coordinates
+- `direction::SVector{3,T}`: Particle direction of motion given as a three dimensional static vector in detector coordinates
+- `time::T`: Time of the event this particle corresponds to, given in unites of ns
+- `energy::T`: Total energy of the particle given in unites of GeV
+- `length::T`: Length of particle track given in unites of m
+- `type::Type{PType}`: Particle type, chosen from any ParticleType subtype (PEPlus, PEMinus, PGamma, PMuPlus, PMuMinus, PNuE, PNuMu, PNuTau, PNuEBar, PNuMuBar, PNuTauBar, PHadronShower, PLightSabre, PUnknown)
+"""
 mutable struct Particle{T,PType<:ParticleType}
     position::SVector{3,T}
     direction::SVector{3,T}
@@ -83,7 +116,27 @@ mutable struct Particle{T,PType<:ParticleType}
     type::Type{PType}
 end
 
-particle_shape(::Particle{T,PType}) where {T, PType} = particle_shape(PType)
+
+"""
+    particle_shape(partricle)
+
+Return struct corresponding to shape of argument-particle's signature
+    
+# Arguments
+- `particle::Type`: Particle to check shape of.
+"""
+particle_shape(::Particle{T,PType}) where {T,PType} = particle_shape(PType)
+
+
+"""
+    is_neutrino(particle)
+
+Return true if particle is of any neutrino subtype (PNuE, PNuEBar, PNuMu, PNuMuBar, PNuTau, PNuTauBar), otherwise return false.
+
+# Arguments
+- `particle::Type`: Particle to check wheather is a neutrino.
+"""
+is_neutrino(::Particle{T,PType}) where {T,PType} = is_neutrino(PType)
 
 
 function Base.convert(::Type{Particle{T}}, x::Particle) where {T}
