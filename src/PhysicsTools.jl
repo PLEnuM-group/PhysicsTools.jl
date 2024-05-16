78de8e809a4ff4c81f1e81ca4deed6c75cc03792
Christian Haack <chr.hck@gmail.come>
2024-05-16 15:17:00 +0200

    pull out proposal interface to separate module

--- src/PhysicsTools.jl
+++ src/PhysicsTools.jl
@@ -8,15 +8,4 @@ include("utils.jl")
 
 @reexport using .Types
 @reexport using .Utils
-
-try
-    include("proposal_interface.jl")
-    @reexport using .ProposalInterface
-catch y
-    @warn "Could not load proposal interface: $y"
-end
-
-@reexport using .ProposalInterface
-
-
 end
